dd6caffdd2ec871e524cebd5293752aad4821bca
Nitsan Avni <nitsanav@gmail.com>
2022-09-11 15:23:49 +0000

    gitpod shows the diff

--- C/GildedRoseUnitTests.cc
+++ C/GildedRoseUnitTests.cc
@@ -1,9 +1,16 @@
 #define APPROVALS_CPPUTEST_EXISTING_MAIN
+// must come before the CppUTest #includes to avoid some build errors
 #include "ApprovalTests.hpp"
+
+using namespace ApprovalTests;
+
+#include <memory>
+
 
 #include <CppUTest/TestHarness.h>
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTestExt/MockSupport.h>
+
 
 
 extern "C" {
@@ -28,7 +35,7 @@ TEST(TestGildedRoseGroup, FirstTest)
 
 TEST(TestGildedRoseGroup, ApprovalTestsTest)
 {
-  ApprovalTests::Approvals::verify(42);
+  Approvals::verify(42, Options(*CustomReporter::createForegroundReporter("code", "-d {Received} {Approved}")));
 }
 
 void example()
@@ -49,7 +56,10 @@ int
 main(int ac, char** av)
 {
   ApprovalTests::initializeApprovalTestsForCppUTest();
+  auto gpReporter = CustomReporter::createForegroundReporter("code", "-d");
+  // Approvals::useAsDefaultReporter(std::make_shared<DiffReporter>(gpReporter));
 
+  // does this line cause a failure?
   // TestRegistry::getCurrentRegistry()->resetPlugins();
 
   return CommandLineTestRunner::RunAllTests(ac, av);
