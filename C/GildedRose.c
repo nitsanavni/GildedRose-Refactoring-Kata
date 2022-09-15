b05a7c00363e15c80708d203a19ef4381fcbbd81
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 12:52:17 +0200

    `if (backstage)` with everything

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -43,6 +43,43 @@ void update_item_quality(Item *item) {
     }
 
     if (backstage_passes) {
+        if (backstage_passes) {
+            if (item->quality < 50) {
+                item->quality = item->quality + 1;
+
+                if (item->sellIn < 11) {
+                    if (item->quality < 50) {
+                        item->quality = item->quality + 1;
+                    }
+                }
+                if (item->sellIn < 6) {
+                    if (item->quality < 50) {
+                        item->quality = item->quality + 1;
+                    }
+                }
+            }
+        } else {
+            if (item->quality > 0) {
+                item->quality = item->quality - 1;
+            }
+        }
+
+        item->sellIn = item->sellIn - 1;
+
+        if (item->sellIn < 0) {
+            if (backstage_passes) {
+                item->quality = 0;
+            } else {
+                if (item->quality > 0) {
+                    item->quality = item->quality - 1;
+                }
+            }
+        }
+
+        return;
+    }
+
+    if (backstage_passes) {
         if (item->quality < 50) {
             item->quality = item->quality + 1;
 
