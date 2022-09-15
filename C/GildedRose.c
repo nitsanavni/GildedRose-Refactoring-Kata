2c42599e30efeb37b06baca29a26af852f4dc43c
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 12:49:38 +0200

    rewrite quality arithmetic

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -85,9 +85,11 @@ void update_brie(Item *item) {
         return;
     }
 
-    item->quality = item->quality + 1;
+//    item->quality = item->quality + 1;
 
     if (item->sellIn < 0) {
-        item->quality = item->quality + 1;
+        item->quality += 2;
+    } else {
+        item->quality++;
     }
 }
