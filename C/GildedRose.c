a598be3a9c540e266ea780a27905c8f38cc7f1b1
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 12:48:04 +0200

    simplify

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -85,13 +85,9 @@ void update_brie(Item *item) {
         return;
     }
 
-    if (do_update) {
-        item->quality = item->quality + 1;
-    }
+    item->quality = item->quality + 1;
 
     if (item->sellIn < 0) {
-        if (do_update) {
-            item->quality = item->quality + 1;
-        }
+        item->quality = item->quality + 1;
     }
 }
