acdbc3f927fabbffceb61c9d1429b04ce21956cf
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 12:21:26 +0200

    rm unused declaration

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -33,7 +33,6 @@ update_quality(Item items[], int size)
 void update_item_quality(Item *item) {
     int not_brie = strcmp(item->name, "Aged Brie");
     int not_backstage_passes = strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert");
-    int not_sulfuras = strcmp(item->name, "Sulfuras, Hand of Ragnaros");
     int sulfuras = !strcmp(item->name, "Sulfuras, Hand of Ragnaros");
 
     if (sulfuras) {
