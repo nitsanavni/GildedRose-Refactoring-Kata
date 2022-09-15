2bd33b8da1b4271c972515cea73e31875ac7788b
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 11:41:57 +0200

    green: properly extract `item`

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -26,73 +26,73 @@ update_quality(Item items[], int size)
     
     for (i = 0; i < size; i++)
     {
-        Item item = items[i];
-        if (strcmp(item.name, "Aged Brie") && strcmp(item.name, "Backstage passes to a TAFKAL80ETC concert"))
+        Item * item = items + i;
+        if (strcmp(item->name, "Aged Brie") && strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
         {
-            if (item.quality > 0)
+            if (item->quality > 0)
             {
-                if (strcmp(item.name, "Sulfuras, Hand of Ragnaros"))
+                if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
                 {
-                    item.quality = item.quality - 1;
+                    item->quality = item->quality - 1;
                 }
             }
         }
         else
         {
-            if (item.quality < 50)
+            if (item->quality < 50)
             {
-                item.quality = item.quality + 1;
+                item->quality = item->quality + 1;
 
-                if (!strcmp(item.name, "Backstage passes to a TAFKAL80ETC concert"))
+                if (!strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
                 {
-                    if (item.sellIn < 11)
+                    if (item->sellIn < 11)
                     {
-                        if (item.quality < 50)
+                        if (item->quality < 50)
                         {
-                            item.quality = item.quality + 1;
+                            item->quality = item->quality + 1;
                         }
                     }
 
-                    if (item.sellIn < 6)
+                    if (item->sellIn < 6)
                     {
-                        if (item.quality < 50)
+                        if (item->quality < 50)
                         {
-                            item.quality = item.quality + 1;
+                            item->quality = item->quality + 1;
                         }
                     }
                 }
             }
         }
 
-        if (strcmp(item.name, "Sulfuras, Hand of Ragnaros"))
+        if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
         {
-            item.sellIn = item.sellIn - 1;
+            item->sellIn = item->sellIn - 1;
         }
 
-        if (item.sellIn < 0)
+        if (item->sellIn < 0)
         {
-            if (strcmp(item.name, "Aged Brie"))
+            if (strcmp(item->name, "Aged Brie"))
             {
-                if (strcmp(item.name, "Backstage passes to a TAFKAL80ETC concert"))
+                if (strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
                 {
-                    if (item.quality > 0)
+                    if (item->quality > 0)
                     {
-                        if (strcmp(item.name, "Sulfuras, Hand of Ragnaros"))
+                        if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
                         {
-                            item.quality = item.quality - 1;
+                            item->quality = item->quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    item.quality = item.quality - item.quality;
+                    item->quality = item->quality - item->quality;
                 }
             }
             else
             {
-                if (item.quality < 50)
+                if (item->quality < 50)
                 {
-                    item.quality = item.quality + 1;
+                    item->quality = item->quality + 1;
                 }
             }
         }
