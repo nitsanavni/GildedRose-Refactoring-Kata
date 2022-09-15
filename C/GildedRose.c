1f4174ed3592107c9e1e6e88aabdb8371da0f60a
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 18:48:39 +0200

    wip extract Updater

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -3,7 +3,19 @@
 
 #include "GildedRose.h"
 
-void update_item_quality(Item *item);
+typedef int (*ItsMe)(const Item *);
+
+typedef void (*Update)(Item *);
+
+struct Updater {
+    ItsMe its_me;
+
+    Update update;
+};
+
+int NUM_OF_UPDATERS = 3;
+
+void update_item_quality(Item *item, struct Updater updaters[]);
 
 void update_brie(Item *item);
 
@@ -14,6 +26,10 @@ void clip_quality(Item *item);
 int backstage_passes_qd(int sellIn);
 
 void default_update_item(Item *item);
+
+int is_backstage_passes(const Item *item);
+
+int is_brie(const Item *item);
 
 Item *
 init_item(Item *item, const char *name, int sellIn, int quality) {
@@ -27,30 +43,54 @@ init_item(Item *item, const char *name, int sellIn, int quality) {
 extern char *
 print_item(char *buffer, Item *item) {
     sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
+
+    return buffer;
 }
+
+
+int default_its_me(const Item *i) {
+    return 1;
+};
+
 
 void
 update_quality(Item items[], int size) {
+    struct Updater updaters[NUM_OF_UPDATERS];
+
+    const struct Updater default_updater = {.its_me = default_its_me, .update = default_update_item};
+    const struct Updater backstage_passes_updater = {.its_me = is_backstage_passes, .update = update_backstage_passes};
+    const struct Updater brie_updater = {.its_me = is_brie, .update = update_brie};
+
+    updaters[0] = brie_updater;
+    updaters[1] = backstage_passes_updater;
+    updaters[2] = default_updater;
+
     for (int i = 0; i < size; i++) {
-        update_item_quality(items + i);
+        update_item_quality(items + i, updaters);
     }
 }
 
-void update_item_quality(Item *item) {
+void update_item_quality(Item *item, struct Updater updaters[]) {
     int sulfuras = !strcmp(item->name, "Sulfuras, Hand of Ragnaros");
-    int brie = !strcmp(item->name, "Aged Brie");
-    int backstage_passes = !strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert");
+    int brie = is_brie(item);
 
     if (sulfuras) {
         // do nothing
-    } else if (brie) {
-        update_brie(item);
-    } else if (backstage_passes) {
-        update_backstage_passes(item);
     } else {
-        default_update_item(item);
+        for (int u = 0; u < NUM_OF_UPDATERS; u++) {
+            const struct Updater updater = updaters[u];
+
+            if (updater.its_me(item)) {
+                updater.update(item);
+                break;
+            }
+        }
     }
 }
+
+int is_brie(const Item *item) { return !strcmp(item->name, "Aged Brie"); }
+
+int is_backstage_passes(const Item *item) { return !strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"); }
 
 void default_update_item(Item *item) {
     item->sellIn = item->sellIn - 1;
