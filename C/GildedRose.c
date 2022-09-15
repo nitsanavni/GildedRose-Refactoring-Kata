bd1bdd07627492f43e16fd3d2dca1b33417117c8
Nitsan Avni <nitsanav@gmail.com>
2022-09-15 12:58:25 +0200

    red: reorder lines

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -63,6 +63,8 @@ void update_item_quality(Item *item) {
 }
 
 void update_backstage_passes(Item *item) {
+    item->sellIn = item->sellIn - 1;
+
     if (item->quality < 50) {
         item->quality = item->quality + 1;
 
@@ -77,8 +79,6 @@ void update_backstage_passes(Item *item) {
             }
         }
     }
-
-    item->sellIn = item->sellIn - 1;
 
     if (item->sellIn < 0) {
         item->quality = 0;
