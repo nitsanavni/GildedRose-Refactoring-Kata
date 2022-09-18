cbbde7999aa7790e0143dfb077ba7abd73402777
Nitsan Avni <nitsanav@gmail.com>
2022-09-18 08:38:37 +0200

    one more time

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -3,79 +3,76 @@
 
 #include "GildedRose.h"
 
-void update_item(Item *item);
-
-Item *
-init_item(Item *item, const char *name, int sellIn, int quality) {
+Item*
+init_item(Item* item, const char *name, int sellIn, int quality)
+{
     item->sellIn = sellIn;
     item->quality = quality;
     item->name = strdup(name);
-
+    
     return item;
 }
 
-extern char *
-print_item(char *buffer, Item *item) {
+extern char* 
+print_item(char* buffer, Item* item) 
+{
     sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
 }
 
-void
-update_quality(Item items[], int size) {
+void 
+update_quality(Item items[], int size) 
+{
     int i;
+    
+    for (i = 0; i < size; i++)
+    {
+        int brie = !strcmp(items[i].name, "Aged Brie");
+        int backstage_passes = !strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert");
+        int sulfuras = !strcmp(items[i].name, "Sulfuras, Hand of Ragnaros");
 
-    for (i = 0; i < size; i++) {
-        update_item(items+i);
-    }
-}
+        if (brie || backstage_passes) {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
 
-void update_item(Item *item) {
-    int brie = !strcmp(item->name, "Aged Brie");
-    int backstage_passes = !strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert");
-    int sulfuras = !strcmp(item->name, "Sulfuras, Hand of Ragnaros");
+                if (backstage_passes) {
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
 
-    if (brie || backstage_passes) {
-        if (item->quality < 50) {
-            item->quality = item->quality + 1;
-
-            if (backstage_passes) {
-                if (item->sellIn < 11) {
-                    if (item->quality < 50) {
-                        item->quality = item->quality + 1;
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
                     }
                 }
-
-                if (item->sellIn < 6) {
-                    if (item->quality < 50) {
-                        item->quality = item->quality + 1;
-                    }
-                }
-            }
-        }
-    } else {
-        if (item->quality > 0) {
-            if (sulfuras) {}
-            else {
-                item->quality = item->quality - 1;
-            }
-        }
-    }
-
-    if (!sulfuras) {
-        item->sellIn = item->sellIn - 1;
-    }
-
-    if (item->sellIn < 0) {
-        if (brie) {
-            if (item->quality < 50) {
-                item->quality = item->quality + 1;
             }
         } else {
-            if (backstage_passes) {
-                item->quality = item->quality - item->quality;
+            if (items[i].quality > 0) {
+                if (!sulfuras) {
+                    items[i].quality = items[i].quality - 1;
+                }
+            }
+        }
+
+        if (!sulfuras) {
+            items[i].sellIn = items[i].sellIn - 1;
+        }
+
+        if (items[i].sellIn < 0)
+        {
+            if (brie) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
             } else {
-                if (item->quality > 0) {
-                    if (strcmp(item->name, "Sulfuras, Hand of Ragnaros")) {
-                        item->quality = item->quality - 1;
+                if (backstage_passes) {
+                    items[i].quality = items[i].quality - items[i].quality;
+                } else {
+                    if (items[i].quality > 0) {
+                        if (sulfuras) continue;
+                        items[i].quality = items[i].quality - 1;
                     }
                 }
             }
