eeb44928308a977bb9482e34a9fadd5280845d66
Nitsan Avni <nitsanav@gmail.com>
2022-09-18 12:00:03 +0200

    reset GildedRose.c

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -3,150 +3,97 @@
 
 #include "GildedRose.h"
 
-static void update_item(Item *item);
-
-void update_brie(Item *item);
-
-void update_backstage_passes(Item *item);
-
-void update_sulfuras(Item *item);
-
-void update_regular_item(Item *);
-
-int is_true(const Item *);
-
-int is_brie(const Item *);
-
-int is_backstage_passes(const Item *item);
-
-int is_sulfuras(const Item *item);
-
-typedef void (*const update)(Item *);
-
-typedef const struct {
-    const update update;
-} Updater;
-
-static Updater *get_updater_for(const Item *item);
-
-Item *
-init_item(Item *item, const char *name, int sellIn, int quality) {
+Item*
+init_item(Item* item, const char *name, int sellIn, int quality)
+{
     item->sellIn = sellIn;
     item->quality = quality;
     item->name = strdup(name);
-
+    
     return item;
 }
 
-void
-update_quality(Item items[], int size) {
-    for (int i = 0; i < size; i++) {
-        update_item(items + i);
-    }
+extern char* 
+print_item(char* buffer, Item* item) 
+{
+    sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
 }
 
-static void update_item(Item *item) {
-    get_updater_for(item)->update(item);
-}
-
-typedef int (*const is_item)(const Item *);
-
-typedef const struct {
-    const is_item is_item;
-    const Updater updater;
-} Handler;
-
-static const int NUM_OF_HANDLERS = 4;
-
-Handler *get_handlers() {
-    static const Handler handlers[] = {
-            {is_brie,             {update_brie}},
-            {is_backstage_passes, {update_backstage_passes}},
-            {is_sulfuras,         {update_sulfuras}},
-            // default - keep last
-            {is_true,             {update_regular_item}}
-    };
-
-    return handlers;
-}
-
-Handler *find_handler_for(const Item *item) {
-    Handler *handlers = get_handlers();
-
-    for (int h = 0; h < NUM_OF_HANDLERS; h++) {
-        if (handlers[h].is_item(item)) {
-            return handlers + h;
-        }
-    }
-}
-
-static Updater *get_updater_for(const Item *item) {
-    return &find_handler_for(item)->updater;
-}
-
-int is_true(const Item *item) {
-    (void) item;
-    return 1;
-}
-
-int is_sulfuras(const Item *item) { return !strcmp(item->name, "Sulfuras, Hand of Ragnaros"); }
-
-int is_backstage_passes(const Item *item) { return !strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"); }
-
-int is_brie(const Item *item) { return !strcmp(item->name, "Aged Brie"); }
-
-void update_regular_item(Item *item) {
-    if (item->quality > 0) {
-        item->quality = item->quality - 1;
-    }
-
-    item->sellIn = item->sellIn - 1;
-
-    if (item->sellIn < 0) {
-        if (item->quality > 0) {
-            item->quality = item->quality - 1;
-        }
-    }
-}
-
-void update_sulfuras(Item *item) {
-    (void) item;
-    // do nothing
-}
-
-void update_backstage_passes(Item *item) {
-    if (item->quality < 50) {
-        item->quality = item->quality + 1;
-
-        if (item->sellIn < 11) {
-            if (item->quality < 50) {
-                item->quality = item->quality + 1;
+void 
+update_quality(Item items[], int size) 
+{
+    int i;
+    
+    for (i = 0; i < size; i++)
+    {
+        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+        {
+            if (items[i].quality > 0)
+            {
+                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
+                {
+                    items[i].quality = items[i].quality - 1;
+                }
             }
         }
-        if (item->sellIn < 6) {
-            if (item->quality < 50) {
-                item->quality = item->quality + 1;
+        else
+        {
+            if (items[i].quality < 50)
+            {
+                items[i].quality = items[i].quality + 1;
+
+                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+                {
+                    if (items[i].sellIn < 11)
+                    {
+                        if (items[i].quality < 50)
+                        {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+
+                    if (items[i].sellIn < 6)
+                    {
+                        if (items[i].quality < 50)
+                        {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+                }
             }
         }
-    }
 
-    item->sellIn = item->sellIn - 1;
+        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
+        {
+            items[i].sellIn = items[i].sellIn - 1;
+        }
 
-    if (item->sellIn < 0) {
-        item->quality = item->quality - item->quality;
-    }
-}
-
-void update_brie(Item *item) {
-    if (item->quality < 50) {
-        item->quality = item->quality + 1;
-    }
-
-    item->sellIn = item->sellIn - 1;
-
-    if (item->sellIn < 0) {
-        if (item->quality < 50) {
-            item->quality = item->quality + 1;
+        if (items[i].sellIn < 0)
+        {
+            if (strcmp(items[i].name, "Aged Brie"))
+            {
+                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+                {
+                    if (items[i].quality > 0)
+                    {
+                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
+                        {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    items[i].quality = items[i].quality - items[i].quality;
+                }
+            }
+            else
+            {
+                if (items[i].quality < 50)
+                {
+                    items[i].quality = items[i].quality + 1;
+                }
+            }
         }
     }
 }
