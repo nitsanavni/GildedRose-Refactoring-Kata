c54f1978e5687fda2412e9ae76402bc8275a12bb
Nitsan Avni <nitsanav@gmail.com>
2022-09-18 15:26:35 +0200

    cases

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -1,99 +1,79 @@
 #include <string.h>
-#include <stdio.h>
 
 #include "GildedRose.h"
 
-Item*
-init_item(Item* item, const char *name, int sellIn, int quality)
-{
+void update_item(Item *item);
+
+Item *
+init_item(Item *item, const char *name, int sellIn, int quality) {
     item->sellIn = sellIn;
     item->quality = quality;
     item->name = strdup(name);
-    
+
     return item;
 }
 
-extern char* 
-print_item(char* buffer, Item* item) 
-{
-    sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
+void
+update_quality(Item items[], int size) {
+    for (int i = 0; i < size; i++) {
+        Item *item = items + i;
+        update_item(item);
+    }
 }
 
-void 
-update_quality(Item items[], int size) 
-{
-    int i;
-    
-    for (i = 0; i < size; i++)
-    {
-        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-        {
-            if (items[i].quality > 0)
-            {
-                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
+void update_item(Item *item) {
+    int brie = !strcmp(item->name, "Aged Brie");
+    int backstage = !strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert");
+    int sulfuras = !strcmp(item->name, "Sulfuras, Hand of Ragnaros");
+
+    if (brie) {
+        if (item->quality < 50) {
+            item->quality = item->quality + 1;
+        }
+
+        item->sellIn = item->sellIn - 1;
+
+        if (item->sellIn < 0) {
+            if (item->quality < 50) {
+                item->quality = item->quality + 1;
             }
         }
-        else
-        {
-            if (items[i].quality < 50)
-            {
-                items[i].quality = items[i].quality + 1;
+    } else if (backstage) {
+        if (item->quality < 50) {
+            item->quality = item->quality + 1;
 
-                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
+            if (item->sellIn < 11) {
+                if (item->quality < 50) {
+                    item->quality = item->quality + 1;
+                }
+            }
+            if (item->sellIn < 6) {
+                if (item->quality < 50) {
+                    item->quality = item->quality + 1;
                 }
             }
         }
 
-        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-        {
-            items[i].sellIn = items[i].sellIn - 1;
+        item->sellIn = item->sellIn - 1;
+
+        if (item->sellIn < 0) {
+            item->quality = item->quality - item->quality;
+        }
+    } else if (sulfuras) {
+        // do nothing
+    } else {
+        if (item->quality > 0) {
+            item->quality = item->quality - 1;
         }
 
-        if (items[i].sellIn < 0)
-        {
-            if (strcmp(items[i].name, "Aged Brie"))
-            {
-                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-                }
+        item->sellIn = item->sellIn - 1;
+
+        if (item->sellIn < 0) {
+            if (item->quality > 0) {
+                item->quality = item->quality - 1;
             }
         }
     }
+
 }
+
