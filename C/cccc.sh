7187a2b71c0af161c8fed0da35e195faafaac163
Nitsan Avni <nitsanav@gmail.com>
2022-09-20 14:00:17 +0200

    cccc: nicer output

--- C/cccc.sh
+++ C/cccc.sh
@@ -1,2 +1,11 @@
+rm -rf .cccc
 $CCCC GildedRose.c &> /dev/null
-yq -p=xml '.[].[].member_function.[] | [.lines_of_code.+value,.McCabes_cyclomatic_complexity.+value,.name] | join(", ")' .cccc/anonymous.xml
+echo "loc\tcmplx.\tmethod"
+yq -p=xml -o=json '.[].[].member_function |
+  select(. != ~) |
+  map({"n":.name, "l":.lines_of_code.+value, "c":.McCabes_cyclomatic_complexity.+value}) |
+  sort_by(.l)|
+  reverse' .cccc/anonymous.xml |
+jq "map({\"c\":.c | tonumber, \"l\":.l | tonumber, \"n\":.n | split(\"(\")[0]}) |
+  sort_by(.c) | sort_by(.l) | reverse | map([.l,.c,.n] | join(\"\t\"))" |
+yq '.[]'
