4533e6256768cbc8f84b5a3fc5b92c142f4fedfd
Nitsan Avni <nitsanav@gmail.com>
2022-10-07 15:45:06 +0200

    simplify cccc.sh

--- C/cccc.sh
+++ C/cccc.sh
@@ -1,11 +1,12 @@
 rm -rf .cccc
 $CCCC GildedRose.c &> /dev/null
 echo "loc\tcmplx.\tmethod"
-yq -p=xml -o=json '.[].[].member_function |
-  select(. != ~) |
-  map({"n":.name, "l":.lines_of_code.+value, "c":.McCabes_cyclomatic_complexity.+value}) |
-  sort_by(.l)|
-  reverse' .cccc/anonymous.xml |
-jq "map({\"c\":.c | tonumber, \"l\":.l | tonumber, \"n\":.n | split(\"(\")[0]}) |
-  sort_by(.c) | sort_by(.l) | reverse | map([.l,.c,.n] | join(\"\t\"))" |
+yq -p=xml -o=json .cccc/anonymous.xml | \
+jq '.CCCC_Project.procedural_detail.member_function |
+  map({
+    n:.name | split("(")[0],
+    l:.lines_of_code."+value" | tonumber,
+    c:.McCabes_cyclomatic_complexity."+value" | tonumber}) |
+  sort_by(.c) | sort_by(.l) | reverse | .[0:5] |
+  map([.l,.c,.n] | join("\t"))' |
 yq '.[]'
