fb9567973305e0843e07e586a056f58b5d39d323
Nitsan Avni <nitsanav@gmail.com>
2022-10-07 12:30:59 +0200

    100%

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -1,61 +1,40 @@
 #include <string.h>
-#include <stdio.h>
 
 #include "GildedRose.h"
 
-Item*
-init_item(Item* item, const char *name, int sellIn, int quality)
-{
+Item *
+init_item(Item *item, const char *name, int sellIn, int quality) {
     item->sellIn = sellIn;
     item->quality = quality;
     item->name = strdup(name);
-    
+
     return item;
 }
 
-extern char* 
-print_item(char* buffer, Item* item) 
-{
-    sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
-}
-
-void 
-update_quality(Item items[], int size) 
-{
+void
+update_quality(Item items[], int size) {
     int i;
-    
-    for (i = 0; i < size; i++)
-    {
-        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-        {
-            if (items[i].quality > 0)
-            {
-                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                {
+
+    for (i = 0; i < size; i++) {
+        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
+            if (items[i].quality > 0) {
+                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
                     items[i].quality = items[i].quality - 1;
                 }
             }
-        }
-        else
-        {
-            if (items[i].quality < 50)
-            {
+        } else {
+            if (items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1;
 
-                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
+                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
 
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
@@ -63,34 +42,23 @@ update_quality(Item items[], int size)
             }
         }
 
-        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-        {
+        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
             items[i].sellIn = items[i].sellIn - 1;
         }
 
-        if (items[i].sellIn < 0)
-        {
-            if (strcmp(items[i].name, "Aged Brie"))
-            {
-                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                        {
+        if (items[i].sellIn < 0) {
+            if (strcmp(items[i].name, "Aged Brie")) {
+                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {
+                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
                             items[i].quality = items[i].quality - 1;
                         }
                     }
-                }
-                else
-                {
+                } else {
                     items[i].quality = items[i].quality - items[i].quality;
                 }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
+            } else {
+                if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
                 }
             }
