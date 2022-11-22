a68135ab4d5ee490d2a86268f606dd05a4f98aec
Nitsan Avni <nitsanav@gmail.com>
2022-11-22 18:07:59 +0100

    extract update_brie and is_brie

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -1,10 +1,15 @@
 #define _DEFAULT_SOURCE
 
 #include <string.h>
+#include <stdbool.h>
+#include "GildedRose.h"
 
 #undef _DEFAULT_SOURCE
 
-#include "GildedRose.h"
+
+void update_item(Item *item);
+
+void update_brie(Item *item);
 
 Item *
 init_item(Item *item, const char *name, int sellIn, int quality) {
@@ -17,56 +22,72 @@ init_item(Item *item, const char *name, int sellIn, int quality) {
 
 void
 update_quality(Item items[], int size) {
-    int i;
+    for (int i = 0; i < size; i++) {
+        update_item(items + i);
+    }
+}
 
-    for (i = 0; i < size; i++) {
-        if (strcmp(items[i].name, "Aged Brie") &&
-            strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality > 0) {
-                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
-                    items[i].quality = items[i].quality - 1;
+bool is_brie(const Item *item) { return !strcmp(item->name, "Aged Brie"); }
+
+void update_item(Item *item) {
+    int not_backstage = strcmp(item->name,
+                               "Backstage passes to a TAFKAL80ETC concert");
+    int not_sulfuras = strcmp(item->name, "Sulfuras, Hand of Ragnaros");
+
+    if (is_brie(item)) {
+        update_brie(item);
+    } else {
+        if (not_backstage != 0) {
+            if (item->quality > 0) {
+                if (not_sulfuras) {
+                    item->quality = item->quality - 1;
                 }
             }
         } else {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+            if (item->quality < 50) {
+                item->quality = item->quality + 1;
 
-                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
+                if (item->sellIn < 11) {
+                    if (item->quality < 50) {
+                        item->quality = item->quality + 1;
                     }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
+                }
+                if (item->sellIn < 6) {
+                    if (item->quality < 50) {
+                        item->quality = item->quality + 1;
                     }
                 }
             }
         }
 
-        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
-            items[i].sellIn = items[i].sellIn - 1;
+        if (not_sulfuras) {
+            item->sellIn = item->sellIn - 1;
         }
 
-        if (items[i].sellIn < 0) {
-            if (strcmp(items[i].name, "Aged Brie")) {
-                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
+        if (item->sellIn < 0) {
+            if (not_backstage) {
+                if (item->quality > 0) {
+                    if (not_sulfuras) {
+                        item->quality = item->quality - 1;
                     }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
+                item->quality = item->quality - item->quality;
             }
+        }
+    }
+}
+
+void update_brie(Item *item) {
+    if (item->quality < 50) {
+        item->quality = item->quality + 1;
+    }
+
+    item->sellIn = item->sellIn - 1;
+
+    if (item->sellIn < 0) {
+        if (item->quality < 50) {
+            item->quality = item->quality + 1;
         }
     }
 }
