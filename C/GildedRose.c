52cda102cf37de24ca025c8a95b5e017b0fc9870
Nitsan Avni <nitsanav@gmail.com>
2022-11-22 17:51:54 +0100

    lift up conditional example

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -1,10 +1,35 @@
 #define _DEFAULT_SOURCE
 
 #include <string.h>
+#include <stdbool.h>
 
 #undef _DEFAULT_SOURCE
 
 #include "GildedRose.h"
+
+void a_true_b_true() {}
+
+void a_true_b_false() {}
+
+void a_false_b_true() {}
+
+void a_false_b_false() {}
+
+void lift_up_conditional(bool a, bool b) {
+    if (a) {
+        if (b) {
+            a_true_b_true();
+        } else {
+            a_true_b_false();
+        }
+    } else {
+        if (b) {
+            a_false_b_true();
+        } else {
+            a_false_b_false();
+        }
+    }
+}
 
 Item *
 init_item(Item *item, const char *name, int sellIn, int quality) {
